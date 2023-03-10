c2b6633f79ad6baa85244f46ac031cc90dc37865
Maxim F <maxxis35@gmail.com>
2023-03-10 15:33:59 +0900

    2

--- src/App.js
+++ src/App.js
@@ -1,64 +1,22 @@
 import React, {useEffect, useMemo, useState} from "react";
-import PostFilter from "./components/PostFilter";
-import PostForm from "./components/PostForm";
-import PostsList from "./components/PostsList";
 import "./styles/App.css"
-import MyModal from "./components/UI/Modal/MyModal";
-import MyButton from "./components/UI/button/MyButton";
-import {usePosts} from "./hooks/usePosts";
-import PostService from "./API/PostService";
-import Loader from "./components/UI/loader/Loader";
-import {useFetching} from "./hooks/useFetching";
-import {getPageCount} from "./utils/pages";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import About from "./pages/About";
+import Posts from "./pages/Posts";
 
 function App() {
-  const [posts, setPosts] = useState([]);
-  const [filter, setFilter] = useState({sort:'', query:''});
-  const [modal, setModal] = useState(false);
-  const [totalPages, setTotalPages] = useState(0);
-  const [limit, setLimit] = useState(10);
-  const [page, setPage] = useState(1);
-  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+    return(
+        <BrowserRouter>
+            <div className="navbar">
 
-  const [fetchPosts, isPostsLoading, postError] = useFetching(async ()=>{
-      const response = await PostService.getAll(limit, page);
-      setPosts(response.data);
-      const totalCount = response.headers['x-total-count'];
-      setTotalPages(getPageCount(totalCount, limit));
-  });
-
-
-  useEffect(()=>{
-     fetchPosts();
-    }, []);
-
-  const createPost = (newPost) => {
-    setPosts([...posts, newPost]);
-    setModal(false);
-  }
-  const removePost = (post) => {
-    setPosts(posts.filter(p => p.id !== post.id));
-  }
-
-
-  return (
-    <div className="App">
-        <MyButton onClick={fetchPosts}>Get Posts</MyButton>
-      <MyButton style={{marginTop: '30px'}} onClick={()=>setModal(true)}>Create post</MyButton>
-      <MyModal visible={modal} setVisible={setModal}>
-        <PostForm create={createPost}/>
-      </MyModal>
-
-      <PostFilter filter={filter} setFilter={setFilter}/>
-        {postError && <h1>Error</h1>}
-        {isPostsLoading
-            ? <div style={{display: 'flex', justifyContent: 'center', marginTop:'50px'}} >
-                <Loader/>
             </div>
-            : <PostsList remove={removePost} posts={sortedAndSearchedPosts} title="Posts List JS"/>
-        }
-    </div>
-  );
+            <Routes>
+                <Route path="/" element={<App/>} />
+                <Route path="/posts" element={<Posts/>} />
+                <Route path="/about" element={<About/>} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App;
